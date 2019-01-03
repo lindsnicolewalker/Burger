6e39d14fcb766d7d00274ddc05cbbc5a8476ea32
lindsnicolewalker <linds.nicolewalker@gmail.com>
2019-01-03 09:50:38 -0800

    write insert queries to populate the burger table with 4 entries and run the sql files in command line

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burger (burger_name,devoured) VALUES ('CA Burger', true);
+INSERT INTO burger (burger_name, devoured) VALUES ('Jalapeno', true);
+INSERT INTO burger (burger_name, devoured) VALUES ('Smokehouse BBQ', true);
+INSERT INTO burger (burger_name, devoured) VALUES ('Guacamole', false);
