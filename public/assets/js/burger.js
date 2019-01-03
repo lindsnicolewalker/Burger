a11fdd83374c376f7e2eea83b1009c8768ab8f75
lindsnicolewalker <linds.nicolewalker@gmail.com>
2019-01-03 10:17:13 -0800

    in burger.js in the public directory, send the PUT request, with a promisary function to reload the page to get the updated list of burgers

--- public/assets/js/burger.js
+++ public/assets/js/burger.js
@@ -9,6 +9,19 @@ $(function() {
         devoured: newDevoured
       };
   
+      // Send the PUT request.
+      $.ajax("/api/burger/" + id, {
+        type: "PUT",
+        data: newDevouredState
+      }).then(
+        function() {
+          console.log("changed sleep to", newDevoured);
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  
 
     });
   
